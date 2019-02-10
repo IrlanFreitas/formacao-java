2d62b84cc1b408f0efc419f2321f90c3872b4e27
Irlan <irlan.freitas@hotmail.com>
2019-02-10 11:04:37 -0300

    Java pt 2 - Aula 3:
    como definir métodos com parâmetros e retorno
    como retornar algo usando a palavra chave return
    como usar a referência this para acessar um atributo
    que podemos passar uma referência como parâmetro do método
    métodos são chamadas a partir da referência usando o operador .

--- 1. Java e Orientação a Objetos/02 Intro Orientacao Objetos/bytebank/src/Conta.java
+++ 1. Java e Orientação a Objetos/02 Intro Orientacao Objetos/bytebank/src/Conta.java
@@ -1,8 +1,29 @@
-public class Conta{
+public class Conta {
 
     double saldo;
     int agencia;
     int numero;
     String titular;
+    
+    public void deposita(double valor) {
+    	this.saldo += valor;
+    }
+    
+    public boolean saca(double valor) {
+    	if (this.saldo < valor) {
+			return false;
+		}
+    	this.saldo -= valor;
+    	return true;
+    }
+    
+    public boolean transfere(double valor, Conta destino) {
+    	if (this.saldo >= valor) {
+    		this.saca(valor);
+        	destino.deposita(valor);
+        	return true;
+		}
+    	return false;
+    }
 
 }
---
+++ 1. Java e Orientação a Objetos/02 Intro Orientacao Objetos/bytebank/src/TestaMetodo.java
@@ -0,0 +1,28 @@
+
+public class TestaMetodo {
+	
+	public static void main(String[] args) {
+		Conta contaDoPaulo = new Conta();
+		contaDoPaulo.saldo = 100;
+		contaDoPaulo.deposita(50);
+		System.out.println(contaDoPaulo.saldo);
+		
+		System.out.println(contaDoPaulo.saca(150));
+		System.out.println(contaDoPaulo.saldo);
+		
+		Conta contaDaMarcela = new Conta();
+		contaDaMarcela.deposita(1000);
+		
+		boolean sucessoTransferencia = contaDaMarcela.transfere(300, contaDoPaulo);
+		
+		if (sucessoTransferencia) {
+			System.out.println("Transferencia com sucesso.");
+		} else {
+			System.out.println("Falou dinheiro.");
+		}
+		
+		System.out.println("Saldo da conta Marcela: "+ contaDaMarcela.saldo);
+		System.out.println("Saldo da conta Paulo: "+ contaDoPaulo.saldo);
+	}
+	
+}
